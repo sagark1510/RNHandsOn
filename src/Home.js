169e7c6c7220b9c7951661d2f250845aa967daaa
Sagar Khatri <sagark1510@gmail.com>
2017-04-22 16:54:15 +0530

    Activity indicator added

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Button, AsyncStorage, FlatList, Alert } from 'react-native';
+import { View, Text, Button, AsyncStorage, FlatList, Alert, ActivityIndicator } from 'react-native';
 import { NavigationActions } from 'react-navigation';
 import LogoutButton from './LogoutButton';
 
@@ -12,23 +12,22 @@ class Home extends Component {
   _keyExtractor = (item, index) => item.id;
 
   state = {
-    list: []
+    list: [],
+    loading: true
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(response => {
-      this.setState({ list: response });
-      console.log(response);
+      this.setState({ list: response, loading: false });
     }).catch(error => {
-      this.setState({ list: [] });
+      this.setState({ list: [], loading: false });
       Alert.alert("Error loading", "Something went wrong");
     });
   }
 
   renderItem = ({ item, index }) => {
-
     return (
       <View style={{ padding: 10, borderBottomWidth: 1, borderColor: '#ccc' }}>
         <Text style={{ fontSize: 14, color: 'black' }}>{item.name}</Text>
@@ -37,6 +36,13 @@ class Home extends Component {
   }
 
   render() {
+    if (this.state.loading) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
     return(
       <View style={{ flex: 1 }}>
         <FlatList
