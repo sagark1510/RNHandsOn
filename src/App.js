9b3bfdb3e61d11e6f47e6429455566281f00a087
Sagar Khatri <sagark1510@gmail.com>
2017-04-22 12:38:45 +0530

    Styling component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Image, TextInput, Button } from 'react-native';
+import { View, Text, Image, TextInput, Button, StyleSheet } from 'react-native';
 
 // ES6 Class feature
 class App extends Component {
@@ -28,29 +28,61 @@ class App extends Component {
   // Only Required LifeCycle method by component
   render() {
     return (
-      <View style={{ marginTop: 20 }}>
-        <Text>{this.state.counter}</Text>
-        <Text>Login</Text>
-        <Image
-          source={{ uri: 'https://placehold.it/100x100' }}
-          style={{ height: 100, width: 100 }}
-        />
-        <TextInput
-          placeholder="Email"
-          keyboardType="email-address"
-          style={{ height: 40 }}
-        />
-        <TextInput
-          placeholder="Password"
-          secureTextEntry
-          style={{ height: 40 }}
-        />
-        <Button
-          title="Login"
-        />
+      <View style={styles.container}>
+        <View style={styles.loginBox}>
+          <Text>Login</Text>
+          <Image
+            source={{ uri: 'https://placehold.it/100x100' }}
+            style={{ height: 100, width: 100 }}
+          />
+          <TextInput
+            placeholder="Email"
+            keyboardType="email-address"
+            style={styles.inputText}
+          />
+          <TextInput
+            placeholder="Password"
+            secureTextEntry
+            style={styles.inputText}
+          />
+          <Button
+            title="Login"
+            onPress={() => {}}
+          />
+        </View>
+        <View style={styles.signupBox}>
+          <Button
+            title="Sign up"
+            onPress={() => {}}
+          />
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  inputText: {
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    paddingHorizontal: 10
+  },
+  loginBox: {
+    flex: 2,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 20
+  },
+  signupBox: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  container: {
+    marginTop: 20,
+    flex: 1
+  }
+})
 
 export default App;
