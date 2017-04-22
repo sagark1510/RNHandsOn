c494ae8528c6872edab7a9fded99d43a98aa3064
Sagar Khatri <sagark1510@gmail.com>
2017-04-22 12:25:22 +0530

    Counter example for State

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 import { View, Text, Image, TextInput, Button } from 'react-native';
 
+// ES6 Class feature
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    };
+  }
+
+  // Component LifeCycle method
+  componentDidMount() {
+
+    // Arrow Function
+    setInterval(() => {
+      //ES6 Object desctructuring
+      const { counter } = this.state;
+
+      // Re-render using this.setState
+      this.setState({
+        counter: counter + 1
+      });
+    }, 1000);
+  }
+
+  // Only Required LifeCycle method by component
   render() {
     return (
       <View style={{ marginTop: 20 }}>
+        <Text>{this.state.counter}</Text>
         <Text>Login</Text>
         <Image
           source={{ uri: 'https://placehold.it/100x100' }}
